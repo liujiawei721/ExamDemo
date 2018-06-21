9af7fec32019b7fe8e7b5a302d6a515a63c5f252
liujiawei721 <40262974+liujiawei721@users.noreply.github.com>
2018-06-21 17:24:59 +0800

    commit

--- src/main/java/com/migu/schedule/Schedule.java
+++ src/main/java/com/migu/schedule/Schedule.java
@@ -1,98 +1,99 @@
 package com.migu.schedule;
 
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import com.migu.schedule.constants.ReturnCodeKeys;
 import com.migu.schedule.info.TaskInfo;
-
-import java.util.List;
 
 /*
 *类名和方法不能修改
  */
 public class Schedule {
 
-	// 
-    public static final String zkhosts = "192.168.2.118:2181";  
-    //   
-    public static final int sessionTimeout = 2000;  
-    // 
-    public static final String parentZnodePath = "/servers"; 
-    
-    
+	Map<Integer, Map<String,Integer>> taskInfoMap1 = new HashMap<Integer, Map<String,Integer>>();
+	Map<Integer, Map<String,Integer>> taskInfoMap2 = new HashMap<Integer, Map<String,Integer>>();
+	
     public int init() {
-        
+    	taskInfoMap1.clear();
+    	taskInfoMap2.clear();
         return ReturnCodeKeys.E001;
     }
 
 
     public int registerNode(int nodeId) {
-    	TaskInfo taskInfo =new TaskInfo();
-    	if(nodeId<0){
+    	Map<String,Integer> taskInfo = new HashMap<String,Integer>();
+    	if(nodeId > 0){
+    		if(taskInfoMap1.containsKey(nodeId)){
+    			return ReturnCodeKeys.E005;
+    		}
+    		taskInfo.put("nodeId", nodeId);
+    		taskInfoMap1.put(nodeId, taskInfo);
+    		return ReturnCodeKeys.E003;
+    	}else{
     		return ReturnCodeKeys.E004;
-    	}else {
-    		taskInfo.setNodeId(nodeId);
     	}
-    	
-    	String path = "/" + nodeId;  
-        String createPath = zk.create(path, null/*data*/, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);  
-        System.out.println(createPath); 
-    	
-        return ReturnCodeKeys.E003;
     }
 
     public int unregisterNode(int nodeId) {
-       
-    	if(nodeId<0){
-    		return ReturnCodeKeys.E004;
-    	}else{
-    		 String path = "/" + nodeId;  
-    	        try {  
-    	            List<String> nodes = zk.getChildren(path, false);  
-    	            for (String node : nodes) {  
-    	                zk.delete(path + "/" + node, -1);  
-    	            }  
-    	            zk.delete(path, -1);  
-    	        } catch (Exception e) {  
-    	            e.printStackTrace();  
-    	        }
-    	}
-        return ReturnCodeKeys.E006;
+    	if (nodeId > 0) {
+            if (taskInfoMap1.containsKey(nodeId)) {
+            	taskInfoMap1.remove(nodeId);
+                return ReturnCodeKeys.E006;
+            }else {
+                return ReturnCodeKeys.E007;
+            }
+        }else{
+            return ReturnCodeKeys.E004;
+        }
     }
 
 
     public int addTask(int taskId, int consumption) {
-    	TaskInfo taskInfo =new TaskInfo();
-    	if(nodeId<0){
+    	Map<String,Integer> taskInfo = new HashMap<String,Integer>();
+    	if(taskId > 0){
+    		if(taskInfoMap2.containsKey(taskId)){
+    			return ReturnCodeKeys.E010;
+    		}
+    		taskInfo.put("consumption", consumption);
+    		taskInfo.put("taskId", taskId);
+    		taskInfoMap2.put(taskId, taskInfo);
+    		return ReturnCodeKeys.E008;
+    	}else{
     		return ReturnCodeKeys.E009;
     	}
-    	taskInfo.setTaskId(taskId);
-        return ReturnCodeKeys.E008;
     }
 
 
     public int deleteTask(int taskId) {
-    	if(nodeId<0){
+    	if(taskId > 0){
+    		if(taskInfoMap2.containsKey(taskId)){
+    			taskInfoMap2.remove(taskId);
+    			return ReturnCodeKeys.E011;
+    		}else{
+    			return ReturnCodeKeys.E012;
+    		}
+    	}else{
     		return ReturnCodeKeys.E009;
     	}
-        return ReturnCodeKeys.E011;
     }
 
 
     public int scheduleTask(int threshold) {
-        if(threshold<0){
-        	return ReturnCodeKeys.E002;
-        }
-        return ReturnCodeKeys.E013;
+    	if(threshold > 0){
+    		
+    		return ReturnCodeKeys.E013;
+    	}else{
+    		return ReturnCodeKeys.E002;
+    	}
+        
     }
 
 
     public int queryTaskStatus(List<TaskInfo> tasks) {
-        if(tasks=null){
-        	return ReturnCodeKeys.E016;
-        }
-        for(TaskInfo a:tasks){
-        	a.toString();
-        }
+    	
         return ReturnCodeKeys.E015;
     }
 
